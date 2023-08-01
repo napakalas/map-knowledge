3f2518d171ee15edbed2c540637aa6f19a4d7575
napakalas <yuda.munarko@gmail.com>
2023-08-01 12:23:01 +1200

    fixed some functionalities

--- tools/compare.py
+++ tools/compare.py
@@ -27,8 +27,10 @@ class ComparedModel:
     def __init__(self, endpoint, model) -> None:
         if endpoint in SCKAN:
             self.ep = KnowledgeStore(store_directory=None, clean_connectivity=False, scicrunch_release=SCKAN[endpoint])
+            self.released = self.ep.scicrunch.sckan_build()['released']
         elif endpoint == 'npo':
             self.ep = NPOExplorer()
+            self.released = self.ep.metadata('NPO')
         else:
             raise argparse.ArgumentError(f'Incorrect endpoint argument: {endpoint}')
         
@@ -39,7 +41,6 @@ class ComparedModel:
             raise Exception(f'No neurons for {model}')
         else:
             neurons = [n['id'] for n in neurons['paths']]
-            # neurons = ['ilxtr:neuron-type-keast-11']
 
         self.ent_knowledges = {n_id:self.ep.entity_knowledge(n_id) for n_id in tqdm(neurons)}
         self.G_conns = {n_id:self.__const_connectivity_graph(conn) for n_id, conn in self.ent_knowledges.items()}
@@ -54,7 +55,7 @@ class ComparedModel:
 
     def __remap_nodes(self, G1, G2):
         maps = {}
-        counter = 1
+        counter = 0
         g1_diff = G1.nodes() - G2.nodes()
         g2_diff = G2.nodes() - G1.nodes()
         for n in G1.nodes() & G2.nodes():
@@ -89,48 +90,70 @@ class ComparedModel:
                 counter += 1
                 g2_diff = set(g2_diff) - {candidate}
                 candidate = None
-                
+
+        # recheck, in not isomorphyc case, there are nodes not mapped   
+        for n in g1_diff:
+            if n not in maps.values():
+                maps[counter] = [n, None]
+                counter += 1
+        for n in g2_diff:
+            if n not in maps.values():
+                maps[counter] = [None, n]
+                counter += 1
         return maps
 
     def compare(self, c_model):
-        self.results['comparing'] = f'{self.ep_name} vs {c_model.ep_name}'
+        self.results['comparing'] = f'{self.ep_name} at {self.released} vs {c_model.ep_name} at {c_model.released}'
         self.results['paths'] = []
-        for n_id, G in self.G_conns.items():
+
+        # comparing the same neurons in this obj and c_model
+        for n_id in set(self.G_conns.keys()) & set(c_model.G_conns.keys()):
+            G = self.G_conns.get(n_id)
             G_to = c_model.G_conns.get(n_id)
             
             # compare isomorphic, for graph structure
-            is_struct_sim = nx.is_isomorphic(G, G_to)
+            is_isomorphic = nx.is_isomorphic(G, G_to)
             map_nodes = self.__remap_nodes(G, G_to)
 
             # strore initial results
-            result = {'id': n_id, 'is_isomorphic': True if is_struct_sim else False}
-
-            if not is_struct_sim:
+            result = {'id': n_id, 'is_isomorphic': True if is_isomorphic else False}
+            if not is_isomorphic:
                 # relable graph
                 G_map = {v[0]:k for k, v in map_nodes.items()}
                 G = nx.relabel_nodes(G, G_map)
                 G_to_map = {v[1]:k for k, v in map_nodes.items()}
                 G_to = nx.relabel_nodes(G_to, G_to_map)
-                # get edge difference
                 G_edge_diffs = G.edges() - G_to.edges()
                 G_edge_to_diffs = G_to.edges() - G.edges()
                 
                 # record the difference
-                result['edges'] = []
-                result['edges'] += [{self.ep_name:(map_nodes[e[0]][0], map_nodes[e[1]][0])} for e in G_edge_diffs]
+                result['edges'] = [{self.ep_name:(map_nodes[e[0]][0], map_nodes[e[1]][0])} for e in G_edge_diffs]
                 result['edges'] += [{c_model.ep_name:(map_nodes[e[0]][1], map_nodes[e[1]][1])} for e in G_edge_to_diffs]
 
             # store map_nodes to results:
             result['nodes'] = [{self.ep_name:ng, c_model.ep_name:nt} for ng, nt in map_nodes.values() if ng != nt]
 
             self.results['paths'] += [result]
+
+        # recording neurons available in this obj but not in c_model
+        for n_id in set(self.G_conns.keys()) - set(c_model.G_conns.keys()):
+            result = {'id': n_id, 'is_isomorphic': False if is_isomorphic else False, 'status':f'not found in {self.ep_name}'}
+            result['edges'] = [{self.ep_name:e} for e in self.G_conns.get(n_id)]
+            self.results['paths'] += [result]
+        
+        # recording neurons available in c_model but not in this obj
+        for n_id in set(c_model.G_conns.keys()) - set(self.G_conns.keys()):
+            result = {'id': n_id, 'is_isomorphic': False if is_isomorphic else False, 'status':f'not found in {c_model.ep_name}'}
+            result['edges'] = [{c_model.ep_name:e} for e in c_model.G_conns.get(n_id)]
+            self.results['paths'] += [result]
+
         return self.results
     
     def close(self):
         self.ep.close()
 
-def print_tabular(results, endpoint1, endpoint2):
-    print(f"{results['model']}, {endpoint1} edge, {endpoint2} edge")
+def print_tabular(results, kb1:ComparedModel, kb2:ComparedModel):
+    print(f"{results['model']}, {kb1.ep_name} at {kb1.released}, {kb2.ep_name} at {kb2.released}")
     for path in results['paths']:
         for edge in path['edges']:
             for ep, e in edge.items():
@@ -140,7 +163,7 @@ def print_tabular(results, endpoint1, endpoint2):
                     ne.append(f'({str_tup})')
 
                 ne_str = '-'.join(ne)
-                print(f"{path['id']}, {ne_str if ep==endpoint1 else ''}, {ne_str if ep==endpoint2 else ''}")
+                print(f"{path['id']}, {ne_str if ep==kb1.ep_name else ''}, {ne_str if ep==kb2.ep_name else ''}")
 
 def compare_model(model, endpoint1, endpoint2, style):
     kb1 = ComparedModel(endpoint1, model)
@@ -157,7 +180,7 @@ def compare_model(model, endpoint1, endpoint2, style):
         results['paths'] = sanitised_paths
     
     if style == 'tabular':
-        print_tabular(results, endpoint1, endpoint2)
+        print_tabular(results, kb1, kb2)
     else:
         print(results)
 
