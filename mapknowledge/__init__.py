b3e0622056d255feab1ea752cbbc1ebba5459d06
napakalas <yuda.munarko@gmail.com>
2023-09-01 15:51:14 +1200

    NpoSparql has a similar interface to Scicrunch

--- mapknowledge/__init__.py
+++ mapknowledge/__init__.py
@@ -163,7 +163,11 @@ class KnowledgeStore(KnowledgeBase):
             cache_msg = f'with cache {db_name}'
         else:
             cache_msg = f'with no cache'
-        if scicrunch_api is not None:
+        if npo:
+            self.__scicrunch = NpoSparql()
+            built = f" built at {self.__scicrunch.sckan_build()['release']}"
+            scicrunch_msg = f"using NPO{built}"
+        elif scicrunch_api is not None:
             self.__scicrunch = SciCrunch(api_endpoint=scicrunch_api,
                                          scicrunch_release=scicrunch_release,
                                          scicrunch_key=scicrunch_key)
@@ -175,10 +179,6 @@ class KnowledgeStore(KnowledgeBase):
         else:
             self.__scicrunch = None
             scicrunch_msg = 'not using SCKAN'
-        self.__npo_db = NpoSparql() if npo else None
-        if self.__npo_db:
-            built = f" built at {self.__npo_db.npo_released()}"
-            scicrunch_msg = f"using NPO{built}"
         log.info(f'Map Knowledge version {__version__} {cache_msg} {scicrunch_msg}')
         # Optionally clear local connectivity knowledge
         if (self.db is not None and clean_connectivity):
@@ -207,9 +207,7 @@ class KnowledgeStore(KnowledgeBase):
                             left join labels as l on c.model = l.entity order by model
                         ''')} if self.db is not None else {}
 
-            if self.__npo_db is not None:
-                return self.__npo_db.connectivity_models()
-            elif self.__scicrunch is not None:
+            if self.__scicrunch is not None:
                 sckan_models = self.__scicrunch.connectivity_models()
                 if not self.__cleaned_connectivity:
                     model_info = cached_models()
@@ -259,13 +257,10 @@ class KnowledgeStore(KnowledgeBase):
 
         if len(knowledge) == 0 or entity == knowledge.get('label', entity):
 
-            if self.__npo_db is not None: ### and entity.startswith(NPO_NLP_NEURONS):
-                knowledge = self.__npo_db.get_knowledge(entity)
-
-            elif self.__scicrunch is not None:
+            if self.__scicrunch is not None:
                 # Consult SciCrunch if we don't know about the entity
                 knowledge = self.__scicrunch.get_knowledge(entity)
-                if 'connectivity' in knowledge:
+                if 'connectivity' in knowledge and isinstance(self.__scicrunch, SciCrunch):
                     # Get phenotype, taxon, and other metadata
                     knowledge.update(self.__scicrunch.connectivity_metadata(entity))
 
