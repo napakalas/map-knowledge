921479e0197a7d16d74a93f821a3515b30a85387
David Brooks <d.brooks@auckland.ac.nz>
2024-11-29 13:50:47 +1300

    We now publish to `origin`.

--- release.sh
+++ release.sh
@@ -8,8 +8,8 @@ fi
 
 poetry build -f wheel
 
-git push upstream
-git push upstream v$1
+git push origin
+git push origin v$1
 gh release create v$1 --verify-tag --title "Release $1" --notes ""
 gh release upload v$1 dist/mapknowledge-$1-py3-none-any.whl
 
