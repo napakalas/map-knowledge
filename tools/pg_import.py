e74610a1e8d29508f47e5feefb07af7497e49f47
David Brooks <d.brooks@auckland.ac.nz>
2025-02-26 18:12:50 +1300

    Add `pg_import.py` to load a Postgres database with our SCKAN knowledge.

---
+++ tools/pg_import.py
@@ -0,0 +1,263 @@
+#===============================================================================
+#
+#  Flatmap viewer and annotation tools
+#
+#  Copyright (c) 2019-21  David Brooks
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#===============================================================================
+
+from functools import reduce
+import json
+import logging
+import os
+from typing import Optional
+
+
+from pprint import pprint
+
+#===============================================================================
+
+import psycopg as pg
+
+#===============================================================================
+
+from mapknowledge import KnowledgeStore
+
+#===============================================================================
+
+PG_DATABASE = 'map_knowledge'
+
+DEFAULT_STORE = 'knowledgebase.db'
+
+KNOWLEDGE_USER = os.environ.get('KNOWLEDGE_USER')
+KNOWLEDGE_HOST = os.environ.get('KNOWLEDGE_HOST', 'localhost:5432')
+
+#===============================================================================
+
+AXON_ID = 'axon'
+DENDRITE_ID = 'dendrite'
+SOMA_ID = 'soma'
+
+def setup_anatomical_types(cursor):
+#==================================
+    anatomical_types = [
+        (AXON_ID, 'Axon', 'The node is located in an axon'),
+        (DENDRITE_ID, 'Dendrite', 'The node is located in a dendrite'),
+        (SOMA_ID, 'Soma', 'The node is located in a soma'),
+    ]
+    cursor.executemany('INSERT INTO anatomical_types (type_id, label, description) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING',
+                       anatomical_types)
+
+def update_connectivity(cursor, knowledge):
+#==========================================
+    source = knowledge['source']
+    for record in knowledge['knowledge']:
+        if record.get('source') == source:
+            if (connectivity := record.get('connectivity')) is not None:
+                path_id = record['id']
+
+                # Nodes
+                nodes = set(json.dumps(node) for (node, _) in connectivity) | set(json.dumps(node) for (_, node) in connectivity)
+                cursor.executemany('INSERT INTO connectivity_nodes (node_id) VALUES (%s) ON CONFLICT DO NOTHING',
+                                   ((node,) for node in nodes))
+
+                # Node features
+                node_features = [ (node, feature)
+                                        for (node, features) in [(node, json.loads(node)) for node in nodes]
+                                            for feature in [features[0]] + features[1] ]
+                cursor.executemany('INSERT INTO connectivity_node_features (node_id, feature_id) VALUES (%s, %s) ON CONFLICT DO NOTHING',
+                                    node_features)
+                path_nodes = [ (path_id, json.dumps(node_0), json.dumps(node_1)) for (node_0, node_1) in connectivity ]
+
+                # Path edges
+                cursor.execute('DELETE FROM connectivity_path_edges WHERE path_id=%s', (path_id, ))
+                with cursor.copy("COPY connectivity_path_edges (path_id, node_0, node_1) FROM STDIN") as copy:
+                    for row in path_nodes:
+                        copy.write_row(row)
+
+                # Path features
+                path_features = [(path_id, feature) for feature in set([nf[1] for nf in node_features])]
+                cursor.execute('DELETE FROM connectivity_path_features WHERE path_id=%s', (path_id, ))
+                with cursor.copy("COPY connectivity_path_features (path_id, feature_id) FROM STDIN") as copy:
+                    for row in path_features:
+                        copy.write_row(row)
+
+                # Path node types
+                node_types = []
+                node_types.extend([(path_id, json.dumps(node), AXON_ID) for node in record.get('axons', [])])
+                node_types.extend([(path_id, json.dumps(node), DENDRITE_ID) for node in record.get('dendrites', [])])
+                node_types.extend([(path_id, json.dumps(node), SOMA_ID) for node in record.get('somas', [])])
+                cursor.execute('DELETE FROM connectivity_node_types WHERE path_id=%s', (path_id, ))
+                with cursor.copy("COPY connectivity_node_types (path_id, node_id, type_id) FROM STDIN") as copy:
+                    for row in node_types:
+                        copy.write_row(row)
+
+                # Path phenotypes
+                cursor.execute('DELETE FROM connectivity_path_phenotypes WHERE path_id=%s', (path_id, ))
+                with cursor.copy("COPY connectivity_path_phenotypes (path_id, phenotype) FROM STDIN") as copy:
+                    for phenotype in record.get('phenotypes', []):
+                        copy.write_row((path_id, phenotype))
+
+                # Path taxons
+                taxons = record.get('taxons', ['NCBITaxon:40674'])
+                cursor.executemany('INSERT INTO taxons (taxon_id) VALUES (%s) ON CONFLICT DO NOTHING',
+                                   ((taxon,) for taxon in taxons))
+                cursor.execute('DELETE FROM connectivity_path_taxons WHERE path_id=%s', (path_id, ))
+                with cursor.copy("COPY connectivity_path_taxons (path_id, taxon_id) FROM STDIN") as copy:
+                    for taxon in taxons:
+                        copy.write_row((path_id, taxon))
+
+                # Path evidence
+                evidence = record.get('references', [])
+                cursor.executemany('INSERT INTO evidence (evidence_id) VALUES (%s) ON CONFLICT DO NOTHING',
+                                   ((evidence,) for evidence in evidence))
+                cursor.execute('DELETE FROM feature_evidence WHERE term_id=%s', (path_id, ))
+                with cursor.copy("COPY feature_evidence (term_id, evidence_id) FROM STDIN") as copy:
+                    for evidence_id in evidence:
+                        copy.write_row((path_id, evidence_id))
+
+                # General path properties
+                cursor.execute('DELETE FROM connectivity_path_properties WHERE path_id=%s', (path_id, ))
+                cursor.execute('INSERT INTO connectivity_path_properties (path_id, biological_sex, alert, disconnected) VALUES (%s, %s, %s, %s)',
+                                   (path_id, record.get('biologicalSex'), record.get('alert'), record.get('pathDisconnected')))
+
+def update_features(cursor, knowledge):
+#======================================
+    source = knowledge['source']
+    cursor.execute('DELETE FROM feature_terms WHERE source_id=%s', (source, ))
+    with cursor.copy("COPY feature_terms (source_id, term_id, label, description) FROM STDIN") as copy:
+        for record in knowledge['knowledge']:
+            if record.get('source') == source:
+                copy.write_row([source, record['id'], record.get('label'), record.get('long-label')])
+    # Need to separately add/update entities with no source
+
+def update_knowledge_source(cursor, source):
+#===========================================
+    cursor.execute('INSERT INTO knowledge_sources (source_id) VALUES (%s) ON CONFLICT DO NOTHING', (source,))
+
+#===============================================================================
+
+def pg_import(knowledge):
+#========================
+    user = f'{KNOWLEDGE_USER}@' if KNOWLEDGE_USER else ''
+    with pg.connect(f'postgresql://{user}{KNOWLEDGE_HOST}/{PG_DATABASE}') as db:
+        with db.cursor() as cursor:
+            # This disables constraints for the session
+            cursor.execute('SET session_replication_role = replica')
+            setup_anatomical_types(cursor)
+            update_knowledge_source(cursor, knowledge['source'])
+            update_features(cursor, knowledge)
+            update_connectivity(cursor, knowledge)
+
+            if (paths := knowledge.get('paths')) is not None:
+                pass
+
+
+
+
+
+        db.commit()
+
+
+"""
+
+        knowledge = {
+            'id': entity
+        }
+            knowledge['label'] = self.__npo_terms[entity]
+
+        # check if entity is a connectivity model
+
+
+        if entity in self.connectivity_models():
+
+
+            knowledge['paths'] = [{'id': v['id'], 'models': v['id']} for v in self.__npo_knowledge.values() if v['class'] == entity]
+            knowledge['references'] = []
+
+
+        # check if entity is a connecitvity path
+        if (path_kn:=self.__npo_knowledge.get(entity)) is not None:
+
+
+
+
+
+
+
+
+{'id': 'ilxtr:NeuronAacar',
+ 'label': 'ilxtr:NeuronAacar',
+ 'paths': [{'id': 'ilxtr:neuron-type-aacar-9a',
+                        'models': 'ilxtr:neuron-type-aacar-9a'},
+                     {'id': 'ilxtr:neuron-type-aacar-7a',
+                        'models': 'ilxtr:neuron-type-aacar-7a'},
+                     {'id': 'ilxtr:neuron-type-aacar-8a',
+                        'models': 'ilxtr:neuron-type-aacar-8a'},
+
+
+
+"""
+
+#===============================================================================
+
+def get_knowledge(args):
+        knowledge_source = args.source
+        store = KnowledgeStore(
+                store_directory=args.store_directory,
+                knowledge_base=args.knowledge_store,
+                read_only=True,
+                knowledge_source=knowledge_source)
+        if store.db is None:
+                raise IOError(f'Unable to open knowledge store {args.store_directory}/{args.knowledge_store}')
+        knowledge_source = store.source
+        if knowledge_source is None:
+                raise ValueError(f'No valid knowledge sources in {args.store_directory}/{args.knowledge_store}')
+
+        knowledge = {
+                'source': knowledge_source,
+                'knowledge': []
+        }
+        for row in store.db.execute('select entity, knowledge from knowledge where source=?', (knowledge_source,)).fetchall():
+                entity_knowledge = json.loads(row[1])
+                entity_knowledge['id'] = row[0]
+                knowledge['knowledge'].append(entity_knowledge)
+        store.close()
+
+        return knowledge
+
+#===============================================================================
+
+def main():
+        import argparse
+
+        parser = argparse.ArgumentParser(description='Import SCKAN knowledge into a PostgresQL knowledge store.')
+        parser.add_argument('--store-directory', required=True, help='Directory containing a knowledge store')
+        parser.add_argument('--knowledge-store', default=DEFAULT_STORE, help=f'Name of knowledge store file. Defaults to `{DEFAULT_STORE}`')
+        parser.add_argument('-q', '--quiet', action='store_true', help='Suppress INFO log messages')
+        parser.add_argument('--source', help='Knowledge source to import; defaults to the most recent source in the store.')
+
+        args = parser.parse_args()
+        if not args.quiet:
+                logging.basicConfig(level=logging.INFO)
+        pg_import(get_knowledge(args))
+
+#===============================================================================
+
+if __name__ == '__main__':
+#=========================
+        main()
+
+#===============================================================================
